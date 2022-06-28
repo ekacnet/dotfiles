e66252129a2f07f9edf2254e597a5d38d97720f9
Matthieu Patou <mat@matws.net>
2022-06-27 18:10:07 -0700

    Add a script to update/install vim plugins

---
+++ dotfiles/vim/bin/update.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Usage: ./update.sh repo
+
+set -e
+dir_bundle=$HOME/.vim/bundle
+dir_autload=$HOME/.vim/autoload
+
+
+repo=$1
+plugin="$(basename $repo | sed -e 's/\.git$//')"
+
+if [ "$plugin" = "vim-pathogen" ]; then
+  dir=$dir_autload
+else
+  dir=$dir_bundle
+fi
+
+
+mkdir -p "$dir"
+[ "$plugin" = "vim-styled-jsx" ] && plugin="000-vim-styled-jsx" # https://goo.gl/tJVPja
+dest="$dir/$plugin"
+rm -rf "$dest"
+(
+  git clone --depth=1 -q "https://github.com/$repo" "$dest"
+  rm -rf "$dest/.git"
+  echo "· Cloned $repo"
+  [ "$plugin" = "onehalf" ] && (mv "$dest" "$dest.TEMP" && mv "$dest.TEMP/vim" "$dest" && rm -rf "$dest.TEMP")
+  [ "$plugin" = "vim-pathogen" ] && (mv "$dest/autoload/pathogen.vim" "$dir" && rm -rf "$dest")
+) &
+wait
